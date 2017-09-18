9d44e1130e07ed5e234231fa4dad4157fad9dfa5
Dave Guymon <davidwguymon@gmail.com>
2017-09-18 14:08:30 -0600

    Initialized create-react-app and npm installed node-sass-chokidar. npm start now watches css files for sass.

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export default class Navbar extends Component {
+
+
+  render(){
+    return (
+      <main className="navbar">
+        <p>Navbar component here</p>
+      </main>
+    )
+  }
+}
