d7cff38d9178cf3c4dee60cccf08aaeb96a401be
Dave Guymon <davidwguymon@gmail.com>
2017-09-18 15:32:35 -0600

    Completed main styling for Navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,17 @@ export default class Navbar extends Component {
   render(){
     return (
       <main className="navbar">
-        <p>Navbar component here</p>
+        <div className="logoContainer"></div>
+        <div className="navLinksContainer">
+          <p className="navLink">Onsite <i className="fa fa-caret-down" aria-hidden="true"></i></p>
+          <p className="navLink">Remote <i className="fa fa-caret-down" aria-hidden="true"></i></p>
+          <p className="navLink">Prepare <i className="fa fa-caret-down" aria-hidden="true"></i></p>
+          <p className="navLink">Partnerships <i className="fa fa-caret-down" aria-hidden="true"></i></p>
+          <p className="navLink">Outcomes <i className="fa fa-caret-down" aria-hidden="true"></i></p>
+          <p className="navLink">Events <i className="fa fa-caret-down" aria-hidden="true"></i></p>
+          <p className="navLink">Scholarships</p>
+        </div>
+        <div className="button"><p>Apply Now</p></div>
       </main>
     )
   }
