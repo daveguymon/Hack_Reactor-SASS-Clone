8c2deee345dfab93c25f502c92f3405d58dbfa91
Dave Guymon <davidwguymon@gmail.com>
2017-09-20 14:01:21 -0600

    Completed main styling on SplashLocations component. Putting finishing touches on positioning of state images.

--- src/components/SplashLocations.js
+++ src/components/SplashLocations.js
@@ -9,9 +9,28 @@ export default function SplashLocations (props){
       </blockquote>
       <p className="cite">— Rodolfo Yabut, Hack Reactor LA Graduate</p>
       <div className="locations-container">
-        <div className="location california"></div>
-        <div className="location texas"></div>
-        <div className="location newyork"></div>
+        <div className="location california">
+          <div className="california1-overlay">
+            <p>San Francisco<i className="fa fa-circle" aria-hidden="true"></i><br /> Campus <i className="fa fa-angle-right" aria-hidden="true"></i></p>
+          </div>
+
+          <div className="california2-overlay">
+            <p>Los Angeles<i className="fa fa-circle" aria-hidden="true"></i><br /> Campus <i className="fa fa-angle-right" aria-hidden="true"></i></p>
+          </div>
+        </div>
+
+        <div className="location texas">
+          <div className="texas-overlay">
+            <p>Austin<i className="fa fa-circle" aria-hidden="true"></i><br />Campus <i className="fa fa-angle-right" aria-hidden="true"></i></p>
+          </div>
+        </div>
+
+        <div className="location newyork">
+          <div className="newyork-overlay">
+            <p>New York City<i className="fa fa-circle" aria-hidden="true"></i><br /> Campus <i className="fa fa-angle-right" aria-hidden="true"></i></p>
+          </div>
+        </div>
+
         <div className="location remote">
           <div className="remote-overlay">
             <p>Remote Immersive<br /> Alumni from 210+ cities <i className="fa fa-angle-right" aria-hidden="true"></i></p>
