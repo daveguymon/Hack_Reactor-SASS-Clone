d8087adac28b7db2e29996197da18e386131aac3
Dave Guymon <davidwguymon@gmail.com>
2017-09-26 17:17:55 -0600

    Created, styled, and added functionality to Products dropdown in MXNavbar component.

--- src/components/mx/MXNavbar.js
+++ src/components/mx/MXNavbar.js
@@ -21,6 +21,18 @@ export default class MXNavbar extends Component {
     })
   }
 
+  showProducts(){
+    this.setState({
+      hideProducts: false
+    })
+  }
+
+  hideProducts(){
+    this.setState({
+      hideProducts: true
+    })
+  }
+
   render(){
 
     const hideDisplay={display: "none"};
@@ -38,7 +50,9 @@ export default class MXNavbar extends Component {
               onMouseLeave={this.hideSolutions.bind(this)}>
                 <p>SOLUTIONS</p>
               </div>
-              <div className="mx-link">
+              <div className="mx-link"
+              onMouseEnter={this.showProducts.bind(this)}
+              onMouseLeave={this.hideProducts.bind(this)}>
                 <p>PRODUCTS</p>
               </div>
               <p>COMPANY</p>
@@ -55,6 +69,20 @@ export default class MXNavbar extends Component {
             <p>ENRICH DATA</p>
             <p>PRESENT DATA</p>
             <p>ACT ON DATA</p>
+          </div>
+
+          <div className="mx-dropdown products-dropdown" style={this.state.hideProducts ? hideDisplay : null}
+          onMouseEnter={this.showProducts.bind(this)}
+          onMouseLeave={this.hideProducts.bind(this)}>
+            <div className="dropdown-bridge"
+            onMouseEnter={this.showProducts.bind(this)}></div>
+            <p>NEXT-GEN PFM</p>
+            <p>CROSS-PLATFORM<br/>FRAMEWORK</p>
+            <p>DATA AGGREGATION</p>
+            <p>BANKING API</p>
+            <p>FINTECH API</p>
+            <p>DATA ANALYTICS</p>
+            <p>PERSONALIZED MARKETING</p>
           </div>
 
           <div className="mx-demo-button-container">
