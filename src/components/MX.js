7d01611b3230393ac3f74392e010a04e9162a867
Dave Guymon <davidwguymon@gmail.com>
2017-09-29 13:33:55 -0600

    Created and styled MXBanner component.

--- src/components/MX.js
+++ src/components/MX.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import MXNavbar from './mx/MXNavbar';
+import MXBanner from './mx/MXBanner';
 
 export default function MX (props) {
 
   return (
     <div className="MX">
       <MXNavbar />
+      <MXBanner />
     </div>
   );
 }
---
+++ src/components/mx/MXBanner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function MXBanner(props) {
+
+  return(
+    <main className="mx-banner">
+      <div className="mx-banner-content">
+        <h1 className="mx-banner-lg-text">DATA FOR<br /><span>BETTER BANKING</span></h1>
+        <p className="mx-banner-sm-text">With $70 million in total funding and 1,700+ signed clients, MX<br /> enables you to collect, enrich, present, and act on data — giving your<br /> users a 5-star digital experience.</p>
+      </div>
+    </main>
+  )
+}
