2a61a4e7cc81fa19421ec2b337dbee27f82f255b
Dave Guymon <davidwguymon@gmail.com>
2017-10-07 13:58:22 -0600

    Created and styled UberGiftCards component.

--- src/components/Uber.js
+++ src/components/Uber.js
@@ -7,6 +7,7 @@ import UberCommitment from './uber/UberCommitment';
 import FareEstimate from './uber/FareEstimate';
 import ReasonsToDrive from './uber/ReasonsToDrive';
 import UberPartners from './uber/UberPartners';
+import UberGiftCards from './uber/UberGiftCards';
 
 
 
@@ -22,6 +23,7 @@ export default function Uber (props) {
       <FareEstimate />
       <ReasonsToDrive />
       <UberPartners />
+      <UberGiftCards />
     </div>
   );
 }
---
+++ src/components/uber/UberGiftCards.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function UberGiftCards (props) {
+
+  return (
+    <main className="uber-gift-cards">
+      <img src="https://d1a3f4spazzrp4.cloudfront.net/chameleon-assets/v1.0.0/b82c316e-e9b9-4ae1-8fcc-41f13e55d690/desk.jpg" />
+      <div className="gift-cards-text-contents">
+        <h2>Gift cards</h2>
+        <h1>The gift of Uber</h1>
+        <p>Uber gift cards for Uber rides and UberEATS. Add a touch of joy to<br /> someone’s day when you give them the gift of a ride or a meal delivered<br /> right to them.</p>
+        <p className="uber-buy-now">BUY NOW<i className="fa fa-chevron-right" aria-hidden="true"></i></p>
+      </div>
+    </main>
+  )
+}
