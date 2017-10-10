542edadb861de6d093d90302daf421aea5dcda3f
Dave Guymon <davidwguymon@gmail.com>
2017-10-09 21:34:41 -0600

    Created UberFooter component; completed styling of top section of component.

--- src/components/Uber.js
+++ src/components/Uber.js
@@ -11,6 +11,7 @@ import UberGiftCards from './uber/UberGiftCards';
 import UberConversation from './uber/UberConversation';
 import UberFindCity from './uber/UberFindCity';
 import UberSkyline from './uber/UberSkyline';
+import UberFooter from './uber/UberFooter';
 
 
 
@@ -30,6 +31,7 @@ export default function Uber (props) {
       <UberConversation />
       <UberFindCity />
       <UberSkyline />
+      <UberFooter />
     </div>
   );
 }
---
+++ src/components/uber/UberFooter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function UberFooter (props) {
+
+  return (
+    <main className="uber-footer">
+      <section className="uber-footer-buttons">
+        <div className="uber-footer-logo"></div>
+        <div className="uber-footer-signup-btn"><p>SIGN UP TO RIDE</p></div>
+        <div className="uber-footer-drive-btn"><p>BECOME A DRIVER</p></div>
+      </section>
+    </main>
+  )
+}
